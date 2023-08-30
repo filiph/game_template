9f11c1d036d13153cf675b2362188c511b0980e0
Filip Hracek <filip.hracek@gmail.com>
2023-08-30 13:05:13 +0200

    Initial copy

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'router.dart';
+import 'src/app_lifecycle/app_lifecycle.dart';
+import 'src/audio/audio_controller.dart';
+import 'src/player_progress/player_progress.dart';
+import 'src/settings/settings.dart';
+import 'src/style/palette.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppLifecycleObserver(
+      child: MultiProvider(
+        providers: [
+          Provider(create: (context) => Palette()),
+          ChangeNotifierProvider(create: (context) => PlayerProgress()),
+          Provider(create: (context) => SettingsController()),
+          // Set up audio.
+          ProxyProvider2<SettingsController, AppLifecycleStateNotifier,
+              AudioController>(
+            // Ensures that music starts immediately.
+            lazy: false,
+            create: (context) => AudioController(),
+            update: (context, settings, lifecycleNotifier, audio) {
+              audio!.attachDependencies(lifecycleNotifier, settings);
+              return audio;
+            },
+            dispose: (context, audio) => audio.dispose(),
+          ),
+        ],
+        child: Builder(builder: (context) {
+          final palette = context.watch<Palette>();
+
+          return MaterialApp.router(
+            title: 'My Flutter Game',
+            theme: ThemeData.from(
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: palette.darkPen,
+                background: palette.backgroundMain,
+              ),
+              textTheme: TextTheme(
+                bodyMedium: TextStyle(color: palette.ink),
+              ),
+              useMaterial3: true,
+            ).copyWith(
+              // Make buttons more fun.
+              filledButtonTheme: FilledButtonThemeData(
+                style: FilledButton.styleFrom(
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            routeInformationProvider: router.routeInformationProvider,
+            routeInformationParser: router.routeInformationParser,
+            routerDelegate: router.routerDelegate,
+          );
+        }),
+      ),
+    );
+  }
+}
---
+++ lib/src/style/my_button.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  final Widget child;
+
+  final VoidCallback? onPressed;
+
+  const MyButton({super.key, required this.child, this.onPressed});
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 300),
+    vsync: this,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) {
+        _controller.repeat();
+      },
+      onExit: (event) {
+        _controller.stop(canceled: false);
+      },
+      child: RotationTransition(
+        turns: _controller.drive(const _MySineTween(0.005)),
+        child: FilledButton(
+          onPressed: widget.onPressed,
+          child: widget.child,
+        ),
+      ),
+    );
+  }
+}
+
+class _MySineTween extends Animatable<double> {
+  final double maxExtent;
+
+  const _MySineTween(this.maxExtent);
+
+  @override
+  double transform(double t) {
+    return sin(t * 2 * pi) * maxExtent;
+  }
+}
