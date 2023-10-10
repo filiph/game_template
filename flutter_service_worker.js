598c7e5ed2017fd46f55adb6a86b1253fd30a780
Filip Hracek <filip.hracek@gmail.com>
2023-10-10 10:36:55 +0200

    Built web
    Branch: card_game_template
    Commit: aa99af8585d93d09ff6f1bc2761c9af9c7449c6c (dirty)
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4af817f8de4133edacca0908f16858d5",
-"index.html": "66a1fe16a26abc6c8ead723e30f8aed1",
-"/": "66a1fe16a26abc6c8ead723e30f8aed1",
-"main.dart.js": "735d791fe13850e20397460018f34952",
+"index.html": "3efd43be679a5fa4cdbade87132465e8",
+"/": "3efd43be679a5fa4cdbade87132465e8",
+"main.dart.js": "72577f2507a843a3963566c6856b9aa8",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
