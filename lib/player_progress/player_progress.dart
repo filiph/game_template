765cd36a8e33051558c7e9817258f7bc03b888a2
Filip Hracek <filip.hracek@gmail.com>
2023-11-01 14:08:17 +0100

    Standardize settings and player progress

--- lib/player_progress/player_progress.dart
+++ lib/player_progress/player_progress.dart
@@ -13,34 +13,22 @@ import 'persistence/player_progress_persistence.dart';
 class PlayerProgress extends ChangeNotifier {
   static const maxHighestScoresPerPlayer = 10;
 
-  /// TODO: If needed, replace this with some other mechanism for saving
-  ///       the player's progress. Currently, this uses the local storage
-  ///       (i.e. NSUserDefaults on iOS, SharedPreferences on Android
-  ///       or local storage on the web).
-  final PlayerProgressPersistence _store =
-      LocalStoragePlayerProgressPersistence();
+  /// By default, settings are persisted using
+  /// [LocalStoragePlayerProgressPersistence] (i.e. NSUserDefaults on iOS,
+  /// SharedPreferences on Android or local storage on the web).
+  final PlayerProgressPersistence _store;
 
   int _highestLevelReached = 0;
 
   /// Creates an instance of [PlayerProgress] backed by an injected
   /// persistence [store].
-  PlayerProgress() {
-    getLatestFromStore();
+  PlayerProgress({PlayerProgressPersistence? store})
+      : _store = store ?? LocalStoragePlayerProgressPersistence() {
+    _getLatestFromStore();
   }
 
   /// The highest level that the player has reached so far.
   int get highestLevelReached => _highestLevelReached;
-
-  /// Fetches the latest data from the backing persistence store.
-  Future<void> getLatestFromStore() async {
-    final level = await _store.getHighestLevelReached();
-    if (level > _highestLevelReached) {
-      _highestLevelReached = level;
-      notifyListeners();
-    } else if (level < _highestLevelReached) {
-      await _store.saveHighestLevelReached(_highestLevelReached);
-    }
-  }
 
   /// Resets the player's progress so it's like if they just started
   /// playing the game for the first time.
@@ -60,6 +48,17 @@ class PlayerProgress extends ChangeNotifier {
       notifyListeners();
 
       unawaited(_store.saveHighestLevelReached(level));
+    }
+  }
+
+  /// Fetches the latest data from the backing persistence store.
+  Future<void> _getLatestFromStore() async {
+    final level = await _store.getHighestLevelReached();
+    if (level > _highestLevelReached) {
+      _highestLevelReached = level;
+      notifyListeners();
+    } else if (level < _highestLevelReached) {
+      await _store.saveHighestLevelReached(_highestLevelReached);
     }
   }
 }
